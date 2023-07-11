2ef2f987c1a2df71c684f5f315ef073eaa629ce9
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-07-11 15:46:46 +0200

    models

--- src/models/models.py
+++ src/models/models.py
@@ -3,10 +3,10 @@ import uuid
 
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 
-from src.db.database import Base
 
+Base = declarative_base()
 
 class Directory(Base):
     __tablename__ = 'directories'
@@ -19,7 +19,7 @@ class File(Base):
     __tablename__ = 'files'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid1)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False, index=True)
     name = Column(String(125), nullable=False)
     path = Column(String(255), nullable=False, unique=True)
     size = Column(Integer, nullable=False)
@@ -33,5 +33,5 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid1)
     username = Column(String(125), nullable=False, unique=True)
     password = Column(String(125), nullable=False)
-    files = relationship('File', backref='user', cascade='CASCADE')
+    files = relationship('File', backref='user')
     created_at = Column(DateTime, index=True, default=datetime.utcnow)
